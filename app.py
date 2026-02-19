101c82dd94b953182a98a6d7dbb9c51266129ebe
Abhinav2648 <abhinavyadav2648@gmail.com>
2026-02-19 23:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import joblib
 
+# Load model and vectorizer
 model = joblib.load("model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
-st.title("Fake Review Detection")
+st.title("🛍️ Fake Review Detection")
 
 review = st.text_area("Enter your review:")
 
 if st.button("Predict"):
-    vec = vectorizer.transform([review])
-    result = model.predict(vec)[0]
-    
-    if result == 1:
-        st.error("Fake Review ❌")
-    else:
-        st.success("Real Review ✅")
+    if review.strip() != "":
+        vec = vectorizer.transform([review])
+        result = model.predict(vec)[0]
 
+        if result == 1:
+            st.error("Fake Review ❌")
+        else:
+            st.success("Real Review ✅")
+    else:
+        st.warning("Please enter a review")
